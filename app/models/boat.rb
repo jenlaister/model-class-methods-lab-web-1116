7dda4197dee2c0aee14dbafe6a2fb95fbaadab80
Jenny Lai <jenny.lai@flatironschool.com>
2017-01-01 22:04:56 -0500

    boats boats boats

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -2,4 +2,36 @@ class Boat < ActiveRecord::Base
   belongs_to  :captain
   has_many    :boat_classifications
   has_many    :classifications, through: :boat_classifications
+
+  def self.first_five
+    Boat.limit(5)
+  end
+
+  def self.dinghy
+    Boat.where('length < ?', 20)
+  end
+  #SELECT * FROM boats WHERE (length < 20)
+
+  def self.ship
+    Boat.where('length > ?', 20)
+  end
+  #SELECT * FROM boats WHERE (length > 20)
+
+  def self.last_three_alphabetically
+    Boat.order(name: :desc).limit(3)
+  end
+  #SELECT * FROM boats ORDER BY name DESC LIMIT 3
+
+  def self.without_a_captain
+    Boat.where(captain_id: nil)
+  end
+
+  def self.sailboats
+    Boat.joins(:classifications).where(classifications: { name: 'Sailboat'})
+  end
+
+  def self.with_three_classifications
+    Boat.joins(:classifications).group('boats.name').having('count(boats.name) = ?', 3)
+  end
+
 end
--- app/models/captain.rb
+++ app/models/captain.rb
@@ -1,3 +1,23 @@
 class Captain < ActiveRecord::Base
   has_many :boats
+
+  def self.catamaran_operators
+    Captain.joins(boats: :classifications).where("classifications.name = ?", 'Catamaran')
+  end
+
+  def self.sailors
+    Captain.joins(boats: :classifications).where("classifications.name = ?", 'Sailboat').uniq
+  end
+
+  def self.talented_seamen
+    captains_of_sailboats = Captain.joins(boats: :classifications).where("classifications.name =?", 'Sailboat').uniq
+    captains_of_motorboats =Captain.joins(boats: :classifications).where("classifications.name =?", 'Motorboat').uniq
+    Captain.where(id: captains_of_sailboats.where(id: captains_of_motorboats))
+  end
+
+  def self.non_sailors
+    non_sailors = Captain.joins(boats: :classifications).where("classifications.name = ?", 'Sailboat')
+    Captain.where.not(id: non_sailors)
+  end
+
 end
